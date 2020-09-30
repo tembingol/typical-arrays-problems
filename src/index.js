c727757b34f1c183052e6dfb166aff11fb3d2bac
Alexandr Rastorguyev <tembingol@gmail.com>
2020-09-30 17:19:04 +0600

    init: start Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0 ) {
+        return 0
+        }
+        else {
+            var minimum = array[0];
+            array.reduce((accumulator, currentValue) => currentValue < minimum ? minimum = currentValue:0)
+        }      
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0 ) {
+        return 0
+        }
+        else {
+            var maximum = array[0];
+            array.reduce((accumulator, currentValue) => currentValue > maximum ? maximum = currentValue:0)
+        }      
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0 ) {
+        return 0
+        }
+        else {
+            return array.reduce((accumulator, currentValue) => accumulator + currentValue)/array.length
+        }
 }
